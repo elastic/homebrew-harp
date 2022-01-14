a0f46012685c53ac1ec973169987d2b4691ec496
Thibault NORMAND <me@zenithar.org>
2022-01-14 09:05:08 +0100

    feat(harp): v0.2.4

--- Formula/harp.rb
+++ Formula/harp.rb
@@ -6,73 +6,41 @@ class Harp < Formula
   desc "Secret management toolchain"
   homepage "https://github.com/elastic/harp"
   license "Apache-2.0"
-  stable do
-    on_macos do
-      if Hardware::CPU.intel?
-        url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.3/harp-darwin-amd64-v0.2.3.tar.xz"
-        sha256 "53a11cdbcf83acfd5bb6187773274c9a73118da7fd4726d562822b29c4ee6a94"
-      elsif Hardware::CPU.arm?
-        url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.3/harp-darwin-arm64-v0.2.3.tar.xz"
-        sha256 "84fae0f7755d20452deb0786c92acc44b6689bc52a6396c2b8bf1630288310cc"
-      end
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.4/harp-darwin-amd64.tar.gz"
+      sha256 "c6f097b7c17ec91d5ce316ec4d0cd1bf796491e8a2547481d92f709dbe4964dd"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.4/harp-darwin-arm64.tar.gz"
+      sha256 "32d098c67d7606caa3b1e27c2829ffff6c829e18aeb67718b61cd5a2d969c342"
     end
-    on_linux do
-      if Hardware::CPU.intel?
-        if Hardware::CPU.is_64_bit?
-          url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.3/harp-linux-amd64-v0.2.3.tar.xz"
-          sha256 "d1d09eda33bd8f7250a81632f4cbf92e3332c2baba7c6aa13eae91f735261f27"
-        end
-      elsif Hardware::CPU.arm?
-        if Hardware::CPU.is_64_bit?
-          url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.3/harp-linux-arm64-v0.2.3.tar.xz"
-          sha256 "425d91a859d26e6edbf0f780805150fb1c1daa72668e2229d3c92bfe66f74ef3"
-        else
-          url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.3/harp-linux-arm7-v0.2.3.tar.xz"
-          sha256 "5482383bbef475b2bc681aa35f3c5637a0451822744951da42d6698f14ffbf58"
-        end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.4/harp-linux-amd64.tar.gz"
+        sha256 "b29e5e1691be09957d4869d19ec03b2a823c3b41fd59b853938a816fceb93c0c"
+      end
+    elsif Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.4/harp-linux-arm64.tar.gz"
+        sha256 "c9fc23bcb67570cf5d19202f17248bf2bb67e0bc4f8b81f85ac7581986adba18"
+      else
+        url "https://github.com/elastic/harp/releases/download/cmd%2Fharp%2Fv0.2.4/harp-linux-arm7.tar.gz"
+        sha256 "efafeb640d0e0574824dde7ba024345ed871528435957a8d6dfbe98fbd1c3964"
       end
     end
   end
 
-  # Source definition
-  head do
-    url "https://github.com/elastic/harp.git", branch: "main"
-
-    # build dependencies
-    depends_on "go" => :build
-    depends_on "mage" => :build
-  end
+  conflicts_with "harp-fips", because: "harp-fips also ships a 'harp' binary"
 
   def install
     ENV.deparallelize
 
-    if build.head?
-      # Prepare build environment
-      ENV["GOPATH"] = buildpath
-      (buildpath/"src/github.com/elastic/harp").install Dir["{*,.git,.gitignore}"]
-
-      # Mage tools
-      ENV.prepend_path "PATH", buildpath/"tools/bin"
-
-      # In github.com/elastic/harp command
-      cd "src/github.com/elastic/harp/cmd/harp" do
-        system "go", "mod", "vendor"
-        system "mage", "compile"
-      end
-
-      # Install builded command
-      cd "src/github.com/elastic/harp/cmd/harp/bin" do
-        # Install binaries
-        if OS.mac? && Hardware::CPU.arm?
-          bin.install "harp-darwin-arm64" => "harp"
-        elsif OS.mac?
-          bin.install "harp-darwin-amd64" => "harp"
-        elsif OS.linux?
-          bin.install "harp-linux-amd64" => "harp"
-        end
-      end
-    elsif OS.mac? && Hardware::CPU.arm?
-      # Install binaries
+    # Install binaries
+    if OS.mac? && Hardware::CPU.arm?
       bin.install "harp-darwin-arm64" => "harp"
     elsif OS.mac?
       bin.install "harp-darwin-amd64" => "harp"
